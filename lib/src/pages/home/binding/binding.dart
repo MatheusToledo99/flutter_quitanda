6b07955ed09a6573b4a23c07a068c617a4e72274
Matheus Toledo <matheus-mjtf-@hotmail.com>
2022-08-15 10:00:14 -0300

    Injetando Dependência - HomeController

--- lib/src/pages/home/binding/binding.dart
+++ lib/src/pages/home/binding/binding.dart
@@ -1,8 +1,9 @@
 import 'package:get/get.dart';
+import 'package:quitanda/src/pages/home/controller/home_controller.dart';
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
-    // TODO: implement dependencies
+    Get.put(HomeController());
   }
 }
