2b43dfa71e78f72c52d1aa8c7bb974417cd6b4e6
Matheus Toledo <matheus-mjtf-@hotmail.com>
2022-08-15 18:09:03 -0300

    Conversão fromJson e toJson

--- lib/src/models/item/item_model.dart
+++ lib/src/models/item/item_model.dart
@@ -1,15 +1,33 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'item_model.g.dart';
+
+@JsonSerializable()
 class ItemModel {
+  String id;
+
+  //No back-end o itemName é title
+  @JsonKey(name: 'title')
+  //
   String itemName;
+  //No back-end o imgUrl é picture
+  @JsonKey(name: 'picture')
   String imgUrl;
   String unit;
   double price;
   String description;
 
   ItemModel({
+    this.id = '',
     required this.itemName,
     required this.imgUrl,
     required this.unit,
     required this.price,
     required this.description,
   });
+
+  factory ItemModel.fromJson(Map<String, dynamic> json) =>
+      _$ItemModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ItemModelToJson(this);
 }
