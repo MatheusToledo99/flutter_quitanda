8f1f7b9ae618b1c61eb1c455d9333ac643870f84
Matheus Toledo <matheus-mjtf-@hotmail.com>
2022-08-16 08:35:51 -0300

    Implementação item - FromJson e ToJson

--- lib/src/models/item/item_model.dart
+++ lib/src/models/item/item_model.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:json_annotation/json_annotation.dart';
 
 part 'item_model.g.dart';
@@ -30,4 +31,9 @@ class ItemModel {
       _$ItemModelFromJson(json);
 
   Map<String, dynamic> toJson() => _$ItemModelToJson(this);
+
+  @override
+  String toString() {
+    return 'ItemModel(id: $id, itemName: $itemName, imgUrl: $imgUrl, unit: $unit, price: $price, description: $description)';
+  }
 }
