b700689f9d8302fa910e6a2d04f158e656b80c9d
Matheus Toledo <matheus-mjtf-@hotmail.com>
2022-08-22 18:10:04 -0300

    Adicionando novas rotas

--- lib/src/pages_routes/app_pages.dart
+++ lib/src/pages_routes/app_pages.dart
@@ -5,6 +5,7 @@ import 'package:quitanda/src/pages/base/base_screen.dart';
 import 'package:quitanda/src/pages/base/binding/navigation_binding.dart';
 import 'package:quitanda/src/pages/cart/binding/cart_binding.dart';
 import 'package:quitanda/src/pages/home/binding/home_binding.dart';
+import 'package:quitanda/src/pages/product/product_screen.dart';
 
 abstract class AppPages {
   static final pages = <GetPage>[
@@ -26,6 +27,9 @@ abstract class AppPages {
         CartBinding(),
       ],
     ),
+
+    //Rota para a tela de produto detalhado
+    GetPage(name: PagesRoutes.productRoute, page: () => ProductScreen())
   ];
 }
 
@@ -33,4 +37,5 @@ abstract class PagesRoutes {
   static const String signInRoute = '/signin';
   static const String signUpRoute = '/signup';
   static const String baseRoute = '/';
+  static const String productRoute = '/product';
 }
